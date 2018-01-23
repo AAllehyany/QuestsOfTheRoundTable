83c9dfff7a07fbb88de619c4111e49bf4a1c2bcf
Kendall Wen <kendall.wen4@gmail.com>
2018-01-22 20:52:19 -0500

    Update Deck.java

--- src/main/java/group52/comp3004/Deck.java
+++ src/main/java/group52/comp3004/Deck.java
@@ -2,50 +2,46 @@ import java.util.ArrayList;
 import java.util.Stack;
 import java.util.Random;
 
-public class Deck{
-    private static String[] types = {"ADVENTURE", "STORY"};
-    private static int[] sizes = {125, 28};
+public class Deck<Card>{
+
+    public String type; // denote if the deck is the story deck or adventure deck
+    private int size = 0;
+    private ArrayList<Card> contents = new ArrayList<Card>(); // store contents while initially creating the deck
+    public Stack<Card> deck = new Stack<Card>();    // store cards in the deck
+    public ArrayList<Card> discard = new ArrayList<Card>(); // store cards in a discard pile ready to be shuffled back into the deck
     
-    // need to create these two arrays for use later
-    private ArrayList<Card> adventure_deck;
-    private ArrayList<Card> story_deck;
+    public Deck(String t){
+        this.type = t;
+    }
 
-    public String type;
-    private int size;
-    public Stack<Card> deck;
-    public ArrayList<Card> discard;
+    // used solely for the purpose of building the deck at the beginning of the game
+    public add(Card c){
+        this.contents.add(c);
+    }
 
+    // internal function to shufffle cards either at the beginning of the game or when the deck is empty.
     private shuffle(ArrayList<Card> cards){
         Random rnd = new Random();
         Stack<Card> d;
         while(!cards.isEmpty()){
-            d.push(cards[rnd.nextInt(cards.size())];
-        }
-        return d;
-    }
-    
-    public Deck(String t){
-        if t.compareToIgnoreCase(types[0]){
-            this.type = types[0];
-            this.size = sizes[0];
-            this.deck = shuffle(adventure_deck);
-        }else if t.compareToIgnoreCase(types[1]){
-            this.type = types[1];
-            this.size = sizes[1];
-            this.deck = shuffle(story_deck);
-        }else{
-            System.out.println("Wrong type\n");
+            this.deck.push(cards.remove(rnd.nextInt(cards.size())));
+            this.size++;
         }
     }
 
+    // draw a card from the top of the deck and returns the card that is drawn
     public Card draw(){
         if(this.deck.empty()){
-            this.deck = shuffle(this.discard);
-            this.discard.clear();
-            this.size = this.discard.size();
+            shuffle(this.discard);
         }
         this.size--;
         this.discard.add(this.deck.peek());
         return this.deck.pop();
+    }
+
+    // play a card and place it in a deck's discard pile
+    public Card discard(Card c){
+        this.discard.add(c);
+        return c;
     }
 }
