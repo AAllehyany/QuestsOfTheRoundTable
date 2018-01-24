16cbcf2046b939988b099986a5d1404542113be5
robertliu3 <35385299+robertliu3@users.noreply.github.com>
2018-01-24 14:07:35 -0500

    Update AdventureCard.java

--- QuestOftheRoundTable/src/AdventureCard.java
+++ QuestOftheRoundTable/src/AdventureCard.java
@@ -2,19 +2,19 @@
 
 public class AdventureCard extends NewCard{
 
-	private int battlePoint;
+	private String description;
 	public AdventureCard(CardType name) {
 		super(name);
-		this.battlePoint = battlePoint;
+		this.description = null;
 		// TODO Auto-generated constructor stub
 	}
 	
-	public void setBP(int battlePoint) {
-		this.battlePoint= battlePoint;
+	public void setDes(String description) {
+		this.description= description;
 	}
 	
-	public int getBP() {
-		return this.battlePoint;
+	public int getDes() {
+		return this.description;
 	}
 
 }
