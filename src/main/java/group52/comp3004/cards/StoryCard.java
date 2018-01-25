c618d8d866d618661e810ad6b11662a19f2345af
Ahmad <aallehyany@gmail.com>
2018-01-25 15:40:04 -0500

    Refactored

--- src/main/java/group52/comp3004/cards/StoryCard.java
+++ src/main/java/group52/comp3004/cards/StoryCard.java
@@ -1,10 +1,18 @@
-package group52.comp3004.cards;
+package group52.comp3004;
 
-public class StoryCard extends Card{
-
-	public StoryCard(String name) {
+public class StoryCard extends NewCard{
+	private String description;
+	public StoryCard(CardType name) {
 		super(name);
+		this.description =description;
+		// TODO Auto-generated constructor stub
 	}
-
 	
+	public void setDes(String description) {
+		this.description=description;
+		
+	}
+	public String getDes() {
+		return this.description;
+	}
 }
