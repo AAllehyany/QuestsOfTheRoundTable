999f3deb28387e8d728cd0b109f1ec00c2868f53
Ahmad <aallehyany@gmail.com>
2018-01-25 16:07:35 -0500

    Initial game state with turns and players

---
+++ src/main/java/group52/comp3004/game/GameState.java
@@ -0,0 +1,40 @@
+package group52.comp3004.game;
+
+import java.util.List;
+
+import group52.comp3004.players.Player;
+
+public class GameState {
+	
+	private List<Player> players;
+	private int currentTurn;
+	private int currentPlayer;
+	/**
+	 * @param players
+	 */
+	public GameState(List<Player> players) {
+		super();
+		this.players = players;
+		this.currentTurn = 0;
+		this.currentPlayer = 0;
+	}
+	
+	
+	public void nextTurn() {
+		currentTurn = (currentTurn + 1) % players.size();
+		currentPlayer = currentTurn;
+	}
+	
+	public void nextPlayer() {
+		currentPlayer = (currentPlayer + 1) % players.size();
+	}
+	
+	public int getCurrentTurn() {
+		return this.currentTurn;
+	}
+	
+	public int getCurrentPlayer() {
+		return this.currentPlayer;
+	}
+	
+}
