ed2bc767be198e50f9572e334dc1d13d2ad91f12
Sandman444 <aswilson@nexicom.net>
2018-01-27 15:59:55 -0500

    Setting up game layout

--- QuestsOfTheRoundTable/src/main/java/group52/comp3004/View.java
+++ QuestsOfTheRoundTable/src/main/java/group52/comp3004/View.java
@@ -1,16 +1,23 @@
 package group52.comp3004;
 
+import java.awt.Color;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.Group;
+import javafx.scene.paint.*;
+import javafx.scene.shape.*;
 import javafx.stage.Stage;
 
 public class View{
 	//Variables
 	Stage stage;
 	Model model;
+	//window size
+	double w_size = 600;
 	
 	//Constructor
 	public View(Model myModel, Stage myStage) {
@@ -20,20 +27,25 @@ public class View{
 	}
 	
 	public void initView() {
-		stage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+		stage.setTitle("Quest of the Round Table");
+		Group root = new Group();
+        Scene scene = new Scene(root, w_size, w_size);
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        stage.setScene(new Scene(root, 500, 500));
+        stage.setScene(scene);
+        
+        Group playArea1 = new Group();
+        Rectangle area1 = new Rectangle(w_size/3, w_size/6, w_size/3, w_size/6);
+       // area1.setFill(Color.black);
+        //area1.setArcHeight(30);
+        //area1.setArcWidth(30);
+        
+        Rectangle player1 = new Rectangle(4*w_size/6, 0, w_size/6, w_size/6);
+        Rectangle hand1 = new Rectangle(w_size/3, 0, w_size/3, w_size/6);
+        
+        root.getChildren().add(area1);
+        root.getChildren().add(player1);
+        root.getChildren().add(hand1);
+        
         stage.show();
 	}
 }
