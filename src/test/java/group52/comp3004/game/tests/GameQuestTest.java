22325d83bf0ca8d228e454284a8cdb225a9c1d61
Ahmad <aallehyany@gmail.com>
2018-02-01 23:45:22 -0500

    fixed

--- src/test/java/group52/comp3004/game/tests/GameQuestTest.java
+++ src/test/java/group52/comp3004/game/tests/GameQuestTest.java
@@ -3,13 +3,13 @@ package group52.comp3004.game.tests;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import group52.comp3004.cards.Foe;
 import group52.comp3004.cards.QuestCard;
 import group52.comp3004.game.GameQuest;
 import group52.comp3004.game.Stage;
+import group52.comp3004.players.Player;
 
 public class GameQuestTest {
 	
@@ -20,14 +20,14 @@ public class GameQuestTest {
 	@Test
 	public void testHasCorrectNumberOfStages() {
 		journey = new QuestCard("LOL Quest", 3);
-		quest = new GameQuest(journey);
+		quest = new GameQuest(journey, new Player(1));
 		assertEquals(3, quest.getNumStages());
 	}
 	
 	@Test
 	public void testAdvancesStageCorrectly() {
 		journey = new QuestCard("LOL Quest", 3);
-		quest = new GameQuest(journey);
+		quest = new GameQuest(journey, new Player(1));
 		assertEquals(0, quest.getCurrentStage());
 		
 		quest.advanceStage();
@@ -43,7 +43,7 @@ public class GameQuestTest {
 	@Test
 	public void testFoePowerIncreasesInEachStage() {
 		journey = new QuestCard("LOL Quest", 3);
-		quest = new GameQuest(journey);
+		quest = new GameQuest(journey, new Player(1));
 		
 		Foe theGoodGuy = new Foe("I AM GOOD GUY", 10, 20);
 		Foe theBadBoy = new Foe("I AM BAD", 2, 1999);
@@ -59,5 +59,35 @@ public class GameQuestTest {
 		
 		assert(quest.addStage(stage3));
 		assertFalse(quest.addStage(stage3));
+	}
+	
+	
+	@Test 
+	public void testPlaysStage() {
+		journey = new QuestCard("LOL Quest", 3);
+		quest = new GameQuest(journey, new Player(1));
+		
+		Foe theGoodGuy = new Foe("I AM GOOD GUY", 2, 20);
+		Foe theBadBoy = new Foe("I AM BAD", 2, 1999);
+		Foe extremelyNiceFoe = new Foe("EXTREMELY NICE FOE", 6, 200);
+		
+		Stage stage1 = new Stage(theGoodGuy);
+		Stage stage2 = new Stage(theBadBoy);
+		Stage stage3 = new Stage(extremelyNiceFoe);
+		
+		quest.addStage(stage1);
+		quest.addStage(stage2);
+		quest.addStage(stage3);
+		
+		Player p1 = new Player(13);
+		Player p2 = new Player(22);
+		Player p3 = new Player(31);
+		
+		
+		quest.addPlayer(p1);
+		quest.addPlayer(p2);
+		quest.addPlayer(p3);
+		
+		
 	}
 }
