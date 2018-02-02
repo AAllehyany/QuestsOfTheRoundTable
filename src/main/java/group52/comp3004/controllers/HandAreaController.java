acb1fb56b29b66648fd4d30afc1e5e75202b053d
Ahmad <aallehyany@gmail.com>
2018-02-01 23:40:47 -0500

    updates the view and has custom click behaviour

--- src/main/java/group52/comp3004/controllers/HandAreaController.java
+++ src/main/java/group52/comp3004/controllers/HandAreaController.java
@@ -1,22 +1,53 @@
 package group52.comp3004.controllers;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
-import javax.annotation.Resources;
-
+import group52.comp3004.GUI.cards.GameCard;
+import group52.comp3004.cards.AdventureCard;
+import group52.comp3004.cards.Card;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
 
 public class HandAreaController implements Initializable{
 	
-	public HandAreaController() {
-		// TODO Auto-generated constructor stub
-	}
+	@FXML
+	private HBox handContainer;
+
 	
+	private CardClickBehaviour clickBehaviour;
+	/**
+	 * 
+	 */
+	public HandAreaController() {
+		super();
+	}
+
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		System.out.println("Field area crated");
+	}
+	
+	
+	public void updateCards(ArrayList<AdventureCard> cards2) {
+		handContainer.getChildren().clear();
+		handContainer.getChildren().addAll(cards2.stream().map(c -> makeGameCard(c)).collect(Collectors.toList()));
+	}
+
+
+	private GameCard makeGameCard(AdventureCard c) {
+		GameCard gameCard = new GameCard(c);
+		gameCard.setOnMouseClicked(e -> clickBehaviour.handClick((AdventureCard) gameCard.getCard()));
 		
+		return gameCard;
+	}
+
+
+	public void setClickBehaviour(CardClickBehaviour clickBehaviour) {
+		this.clickBehaviour = clickBehaviour;
 	}
 }
