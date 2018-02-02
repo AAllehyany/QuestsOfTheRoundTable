38fa733bf092a446c0263e0e73c33b77c102e2a1
Ahmad <aallehyany@gmail.com>
2018-02-01 23:44:11 -0500

    sets up a temporary playercontroller and wires up events

--- src/main/java/group52/comp3004/controllers/GameController.java
+++ src/main/java/group52/comp3004/controllers/GameController.java
@@ -3,13 +3,14 @@ package group52.comp3004.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javax.annotation.Resources;
-
+import group52.comp3004.cards.AdventureCard;
 import group52.comp3004.game.GameState;
+import group52.comp3004.players.Player;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
 public class GameController implements Initializable {
@@ -17,10 +18,18 @@ public class GameController implements Initializable {
 	private GridPane gamepane;
 	private DoubleProperty stageSize = new SimpleDoubleProperty();
 	
+	@FXML
+	private PlayerAreaController player1AreaController;
+	
+	@FXML
+	private Button dealtoplayer1;
 	GameState model;
 	
 	//Constructor
 	public GameController() {
+		model = new GameState();
+		Player demo = new Player(1234);
+		model.addPlayer(demo);
 		stageSize.set(0);
 	}
 
@@ -31,9 +40,27 @@ public class GameController implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		System.out.println("Game controller created");
+		player1AreaController.setHandClickBehaviour(card -> handCardOnClick(card));
+		dealtoplayer1.setOnAction(e -> dealPlayer(0));
+	}
+	
+	public void playCard(AdventureCard card) {
 		
 	}
+	
+	public void handCardOnClick(AdventureCard card) {
+		Player player = this.model.getPlayerByIndex(0);
+		player.playCardToField(card);
+		this.player1AreaController.update(player.getHand(),player.getField());
+	}
+	
+	public void dealPlayer(int index) {
+		this.model.dealToPlayer(index);
+		Player player = this.model.getPlayerByIndex(0);
+		this.player1AreaController.update(player.getHand(),player.getField());
+	}
+	
 	
 	
 }
