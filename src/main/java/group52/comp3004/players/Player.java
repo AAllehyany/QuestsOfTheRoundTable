528b4f5754ae103bc9e4f8a4d185606306e71623
Ahmad <aallehyany@gmail.com>
2018-02-03 21:17:33 -0500

    added temp, field, game and quest

--- src/main/java/group52/comp3004/players/Player.java
+++ src/main/java/group52/comp3004/players/Player.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import group52.comp3004.cards.AdventureCard;
+import group52.comp3004.cards.Ally;
 import group52.comp3004.cards.Weapon;
+import group52.comp3004.game.GameQuest;
+import group52.comp3004.game.GameState;
 
 public class Player {
 	
@@ -17,6 +20,9 @@ public class Player {
 	private int minShields;
 	private ArrayList<AdventureCard> hand;
 	private ArrayList<AdventureCard> field;
+	private ArrayList<AdventureCard> temp;
+	private GameState game;
+	private GameQuest quest;
 	
 	public Player(Integer id) {
 		this.id = id;
@@ -28,6 +34,8 @@ public class Player {
 		weapons = new ArrayList<Integer>();
 		hand = new ArrayList<>();
 		field = new ArrayList<>();
+		temp = new ArrayList<>();
+		quest = null;
 	}
 	
 	public Integer getId() {
@@ -42,8 +50,25 @@ public class Player {
 		return rank;
 	}
 	
+	public void setGame(GameState game) {
+		this.game = game;
+	}
+	
+	public GameState getGame() {
+		return this.game;
+	}
+	
+	public GameQuest getQuest() {
+		return this.quest;
+	}
+	
+	public void setQuest(GameQuest quest) {
+		this.quest = quest;
+		//quest.addPlayer(this);
+	}
+	
 	public Integer getBattlePoints() {
-		return battlePoints + weapons.stream().mapToInt(Integer::intValue).sum();
+		return battlePoints + temp.stream().mapToInt(c -> c.getBp()).sum() + field.stream().mapToInt(c -> c.getBp()).sum();
 	}
 	
 	public void clearWeapons() {
@@ -87,11 +112,30 @@ public class Player {
 		return this.hand.contains(card);
 	}
 	
-	public void playCardToField(AdventureCard card) {
-		if(card instanceof Weapon && !canPlayWeapon((Weapon) card)) return;
-		//if(!hasCardInHand(card)) return;
+	public void playCardToField(Ally card) {
+		if(!hasCardInHand(card)) return;
 		this.field.add(card);
 		this.hand.remove(card);
+	}
+	
+	public void playToTemp(AdventureCard card) {
+		if(!this.hasCardInHand(card)) return;
+		if(card instanceof Weapon && !canPlayWeapon((Weapon) card)) return;
+		
+		this.temp.add(card);
+		this.hand.remove(card);
+	}
+	
+	public void addField(Ally card) {
+		this.field.add(card);
+	}
+	
+	public void addTemp(AdventureCard card) {
+		this.temp.add(card);
+	}
+	
+	public void clearTemp() {
+		
 	}
 
 	private void updateRank() {
@@ -114,5 +158,10 @@ public class Player {
 	public ArrayList<AdventureCard> getField() {
 		// TODO Auto-generated method stub
 		return this.field;
+	}
+
+	public ArrayList<AdventureCard> getTemp() {
+		// TODO Auto-generated method stub
+		return this.temp;
 	}
 }
