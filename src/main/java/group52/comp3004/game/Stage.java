db661c7e0f834ccda3d864d2ba2ba07510b26033
Ahmad <aallehyany@gmail.com>
2018-02-03 21:17:09 -0500

    test stage field

--- src/main/java/group52/comp3004/game/Stage.java
+++ src/main/java/group52/comp3004/game/Stage.java
@@ -5,16 +5,24 @@ import group52.comp3004.cards.Foe;
 public class Stage {
 	
 	private final Foe foe;
-
+	
+	private final boolean testStage;
+	
 	/**
 	 * @param foe
 	 */
 	public Stage(Foe foe) {
 		super();
 		this.foe = foe;
+		testStage = false;
 	}
 	
 	public int getTotalPower() {
 		return foe.getBp();
 	}
+	
+	public boolean isTestStage() {
+		return testStage;
+	}
+	
 }
