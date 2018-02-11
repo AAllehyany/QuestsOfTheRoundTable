74d02c2f83c2a8b17599a50d5ca75e69e1edc78a
dehaoliu <robertliu3@cmail.carleton.ca>
2018-02-11 17:06:55 -0500

    minor change to quest card

--- src/main/java/group52/comp3004/cards/QuestCard.java
+++ src/main/java/group52/comp3004/cards/QuestCard.java
@@ -3,13 +3,16 @@ package group52.comp3004.cards;
 public class QuestCard extends StoryCard {
 	
 	private final int stages;
+	private String foe;
 	// private final List<Foe> foes;
 	
-	public QuestCard(String name, int stages) {
+	public QuestCard(String name, int stages, String foe) {
 		super(name);
 		this.stages = stages;
+		this.foe = foe;
 	}
 	
 	public int getStages() { return this.stages; }
+	public String getFoe() { return this.foe; }
 
 }
