9518de2f02f01f3756e6d1b75890bc9a816b1a4f
Kendall Wen <kendall.wen4@gmail.com>
2018-02-16 14:42:55 -0500

    GUI Updates

--- src/main/java/group52/comp3004/cards/Amour.java
+++ src/main/java/group52/comp3004/cards/Amour.java
@@ -1,14 +1,15 @@
 package group52.comp3004.cards;
 
 import group52.comp3004.cards.AdventureCard;
+import group52.comp3004.ResourceManager;
 
 public class Amour extends AdventureCard{
 	public String name;
 	private int bp;
 	private int bids;
 	
-	public Amour(String name, int bp, int bids){
-		super(name);
+	public Amour(String name, ResourceManager rm, int bp, int bids){
+		super(name, rm);
 		this.bp = bp;
 		this.bids = bids;
 	}
