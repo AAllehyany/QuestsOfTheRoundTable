4cfe108ea8ae71bc2c0730130d25ac209cada269
Sandman444 <aswilson@nexicom.net>
2018-02-24 18:15:13 -0500

    Fixed Directory Problems?

---
+++ src/main/java/group52/comp3004/cards/Pox.java
@@ -0,0 +1,15 @@
+package group52.comp3004.cards;
+
+import group52.comp3004.game.GameState;
+
+public class Pox implements EventBehaviour{
+	@Override
+	public void handle(GameState gamestate) {
+		for(int i=0;i<gamestate.numPlayers();i++) {
+			if(i != gamestate.getCurrentPlayer()) {
+				//gamestate.getPlayerByIndex(i).removeShield(1);
+			}
+		}
+		
+	}
+}
