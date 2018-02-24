c7cee7e4415849068f94731f30395271cb564a16
Sandman444 <aswilson@nexicom.net>
2018-02-24 17:55:25 -0500

    something

---
+++ QuestsOfTheRoundTable/src/main/java/group52/comp3004/cards/Realm.java
@@ -0,0 +1,18 @@
+package group52.comp3004.cards;
+
+import group52.comp3004.game.GameState;
+
+public class Realm implements EventBehaviour{
+
+	@Override
+	public void handle(GameState gamestate) {
+		for(int j=0;j<2;j++) {
+			for(int i=0;i<gamestate.numPlayers();i++) {
+				gamestate.dealToPlayer(i);
+			}
+		}
+
+		
+	}
+
+}
