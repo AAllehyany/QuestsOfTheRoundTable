620c1e00669d665bca8ee8d9b5ac1221e9ba1071
dehaoliu <277541880robert@gmail.com>
2018-04-08 17:35:12 -0400

    deleted rooms set up

--- src/main/java/group52/comp3004/MainPage.java
+++ src/main/java/group52/comp3004/MainPage.java
@@ -1,6 +1,4 @@
 package group52.comp3004;
-
-import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +9,8 @@ import group52.comp3004.sockets.SocketHandler;
 
 @RestController
 public class MainPage {
-	@Autowired
-	private CopyOnWriteArrayList<SocketHandler> newRoom;
-
 	@RequestMapping("/")
 	public String mainPage() {
 		return "Hello world!";
-	}
-	
-	@RequestMapping("/room")
-	public int getNumRooms() {
-		return newRoom.size();
 	}
 }
