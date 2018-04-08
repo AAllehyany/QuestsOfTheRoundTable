c7cb09036de4f4a33c614fc53750fb9198e4b600
dehaoliu <277541880robert@gmail.com>
2018-04-08 16:24:19 -0400

    create room for a game

--- src/main/java/group52/comp3004/MainPage.java
+++ src/main/java/group52/comp3004/MainPage.java
@@ -1,18 +1,26 @@
 package group52.comp3004;
 
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import group52.comp3004.sockets.SocketHandler;
+
 @RestController
 public class MainPage {
 	@Autowired
-	//private List<Server> newServer;
+	private CopyOnWriteArrayList<SocketHandler> newRoom;
 
 	@RequestMapping("/")
 	public String mainPage() {
 		return "Hello world!";
+	}
+	
+	@RequestMapping("/room")
+	public int getNumRooms() {
+		return newRoom.size();
 	}
 }
